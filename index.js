1d7c036d8fdc505548081f8710b523d77e99fd5e
somanandi <soman8698@gmail.com>
2024-01-15 20:02:32 +0530

    js file change

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
+// java-script code
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 300) {
+    if (scroll >= 200) {
       $(".goodreaders-navbar").addClass("fixed-top");
     } else {
       $(".goodreaders-navbar").removeClass("fixed-top");
